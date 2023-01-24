db4b07c1b595fc7318b463f746ba5e6fa401dfde
Tamutnih <renu07@list.ru>
2023-01-24 22:01:27 +0500

    solution

---
+++ InterfaceAdress.java
@@ -0,0 +1,6 @@
+package main.java.com.example;
+
+public interface InterfaceAdress {
+    public void adressMethod();
+
+}
---
+++ InterfaceTelegram.java
@@ -0,0 +1,5 @@
+package main.java.com.example;
+
+public interface InterfaceTelegram {
+    public void TelegramMethod();
+}
